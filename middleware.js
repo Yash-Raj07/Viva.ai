71cdc63b7eb328670b3c48ac1ab659ed8d32a4c8
Yash Raj <152703900+Yash-Raj07@users.noreply.github.com>
2026-03-07 23:41:53 +0530

    Removed duplicate auth() call

--- middleware.js
+++ middleware.js
@@ -11,10 +11,10 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
+  const authData = await auth();
+  const { userId, redirectToSignIn } = authData;
 
   if (!userId && isProtectedRoute(req)) {
-    const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
 
@@ -23,9 +23,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
